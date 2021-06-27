7deb11804fc3ee7c85803fdc7054b28f2b2d3544
JamesArbogast <jamesarbogast52@gmail.com>
2021-06-27 15:49:17 -0700

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    IDictionary<string, int> cardValues = new Dictionary<string, int>();
+    private enum GameState
+    {
+        INTRO,
+        PREP,
+        CHOOSE,
+        BATTLE,
+        DAMAGE,
+        END
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
