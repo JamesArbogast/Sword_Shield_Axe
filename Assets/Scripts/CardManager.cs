aa64db84efe1422643cb6a38cde5c2c61694f05e
James Arbogast <jamesarbogast52@gmail.com>
2021-07-11 00:16:06 -0700

    Card Values Prep complete

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour
 {
+    private GameManager gameManager;
+    public bool pointsConfirmed;
+    public GameObject upArrow;
+    public GameObject downArrow;
+    public Text numberText;
+    public int cardValue = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        cardValue = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        numberText.text = cardValue.ToString();
+    }
+    //attached to arrows on click event
+    public void UpdateCardValueUp()
+    {
+        if(gameManager.cardPoints > 0)
+        {
+            cardValue += 10;
+            gameManager.cardPoints -= 10;
+        }
+
+        if(gameManager.cardPoints <= 0)
+        {
+            gameManager.cardPoints = 0;
+        }
+    }
+    //attached to arrows on click event
+    public void UpdateCardValueDown()
+    {
+        cardValue -= 10;
+        if(cardValue >= 0)
+        {
+            gameManager.cardPoints += 10;
+        }
+
+        if (cardValue <= 0)
+        {
+            cardValue = 0;
+        }
+
+        if (gameManager.cardPoints >= 100)
+        {
+            gameManager.cardPoints = 100;
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
+    public int cardPoints = 0;
+    public Text cardPointsText;
     IDictionary<string, int> cardValues = new Dictionary<string, int>();
+    public class Player
+    {
+        public string Name { get; set; }
+        public int GreenCard { get; set; }
+        public int BlueCard { get; set; }
+        public int PurpleCard { get; set; }
+        public Player(string name, int greenCard, int blueCard, int purpleCard)
+        {
+            Name = name;
+            GreenCard = greenCard;
+            BlueCard = blueCard;
+            PurpleCard = purpleCard;
+        }
+    }
     private enum GameState
     {
         INTRO,
@@ -21,20 +38,25 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameState = GameState.INTRO;
+        cardPoints = 100;
+        //change this to intro once the intro is done
+        gameState = GameState.PREP;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(gameState);
         switch (gameState)
         {
+            
             case GameState.INTRO:
                 Console.WriteLine("Game state: Intro");
                 //Set up intro to game, cards come out animation. 
                 //Asks if you need a tutorial. 
                 //If its first time go right to tutorial.
                 //Introduce enemy and or opponent.
+                //create players name if it isn't already created
                 break;
             case GameState.PREP:
                 Console.WriteLine("Game state: Prep");
@@ -43,6 +65,7 @@ public class GameManager : MonoBehaviour
                 //Assign power amount to each card in increments of ten. 
                 //They draw from a total amount of power (100)
                 //Add confirmation that the totals are correct.
+                cardPointsText.text = cardPoints.ToString();
                 break;
             case GameState.CHOOSE:
                 Console.WriteLine("Game state: CHOOSE");
@@ -58,5 +81,11 @@ public class GameManager : MonoBehaviour
                 break;
         }
             
+    }
+    
+
+    public void ConfirmCardVals(int greenCard, int blueCard, int purpleCard)
+    {
+        //start HERE tomorrow
     }
 }
