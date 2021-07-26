8da3b614829d060508aacfe213eac0b0f122fb9c
James Arbogast <jamesarbogast52@gmail.com>
2021-07-25 23:24:40 -0700

    Scene Management Started

---
+++ Assets/SceneMgt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMgt : MonoBehaviour
+{
+     public Scene nextScene;
+
+    public void GetNextScene()
+    {
+        SceneManager.LoadScene(nextScene.name);
+    }
+}
